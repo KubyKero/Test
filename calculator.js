e144b8cdc239a7b917b3bdf546b26a3df0e30980
Kerem Kubat <kerokuby@hotmail.com>
2026-02-16 23:26:34 +0100

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,20 @@
+const getal1 = parseFloat(prompt("Eerste getal:"));
+const operatie = prompt("Kies (+, -, *, /):");
+const getal2 = parseFloat(prompt("Tweede getal:"));
+
+let resultaat;
+
+if (operatie === "+") {
+    resultaat = getal1 + getal2;
+} else if (operatie === "-") {
+    resultaat = getal1 - getal2;
+} else if (operatie === "*") {
+    resultaat = getal1 * getal2;
+} else if (operatie === "/") {
+    resultaat = getal2 !== 0 ? getal1 / getal2 : "Kan niet delen door nul!";
+} else {
+    resultaat = "Ongeldige operatie!";
+}
+
+console.log("Resultaat:", resultaat);
+alert("Resultaat: " + resultaat);
